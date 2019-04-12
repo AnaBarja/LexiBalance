a9b11af3257860655924b2ac96d74dffe77171f6
Ana Barja <anabarjafer@gmail.com>
2019-04-12 22:52:50 +0200

    Implementación de los Select para cambiar trabajador, producto y cliente.

--- LexiBalance/Pages/Ventas/Edit.cshtml.cs
+++ LexiBalance/Pages/Ventas/Edit.cshtml.cs
@@ -1,18 +1,20 @@
-﻿using System;
+﻿using LexiBalance.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using LexiBalance.Models;
 
 namespace LexiBalance.Pages.Ventas
 {
     public class EditModel : PageModel
     {
         private readonly LexiBalance.Models.LexiBalanceContext _context;
+
+        public static List<string> productos;
+        public static List<string> trabajadores;
+        public static List<string> clientes;
 
         public EditModel(LexiBalance.Models.LexiBalanceContext context)
         {
@@ -35,6 +37,46 @@ namespace LexiBalance.Pages.Ventas
             {
                 return NotFound();
             }
+
+            productos = new List<string>();
+            trabajadores = new List<string>();
+            clientes = new List<string>();
+
+            using (var connection = _context.Database.GetDbConnection())
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT Nombre FROM Productos";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            productos.Add(reader.GetString(0));
+                        }
+                    }
+
+                    command.CommandText = "SELECT Nombre FROM Trabajador";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            trabajadores.Add(reader.GetString(0));
+                        }
+                    }
+
+                    command.CommandText = "SELECT Nombre FROM Cliente";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            clientes.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+
             return Page();
         }
 
