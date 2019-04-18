c46b0af7ec07a2b542cc2f840591425d4b0d4b6d
Ana Barja <anabarjafer@gmail.com>
2019-04-18 22:08:51 +0200

    Cambios visuales. Trabajador ya no ve para editar, ni crear ni insertar productos. Se ven las últimas ventas y mejor trabajador.

--- LexiBalance/Pages/Index.cshtml.cs
+++ LexiBalance/Pages/Index.cshtml.cs
@@ -1,17 +1,52 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LexiBalance.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly LexiBalance.Models.LexiBalanceContext _context;
+
+        public static string mejorTrabajador;
+        public static List<string> producto;
+        public static List<int> cantidadProducto;
+
+
+        public IndexModel(LexiBalance.Models.LexiBalanceContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            producto = new List<string>();
+            cantidadProducto = new List<int>();
 
+            using (var connection = _context.Database.GetDbConnection())
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT COUNT(Trabajador), Trabajador from Venta group by Trabajador " +
+                        "order by COUNT(Trabajador) desc limit 1;";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        mejorTrabajador = reader.GetString(1);
+                    }
+
+                    command.CommandText = "SELECT Producto, Cantidad from Venta order by Fecha desc limit 3;";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            producto.Add(reader.GetString(0));
+                            cantidadProducto.Add(reader.GetInt32(1));
+                        }
+                    }
+                }
+            }
         }
     }
 }
