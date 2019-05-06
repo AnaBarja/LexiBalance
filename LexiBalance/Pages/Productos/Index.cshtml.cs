3577791c31b90fef6213bd2b2bc366e19bef0539
Ana Barja <anabarjafer@gmail.com>
2019-05-06 07:16:38 +0200

    Añadido botón de búsqueda por nombre del producto.

--- LexiBalance/Pages/Productos/Index.cshtml.cs
+++ LexiBalance/Pages/Productos/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using LexiBalance.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace LexiBalance.Pages.Productos
 {
@@ -10,16 +12,24 @@ namespace LexiBalance.Pages.Productos
     {
         private readonly LexiBalance.Models.LexiBalanceContext _context;
 
+        public IList<Producto> Productos { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Buscar { get; set; }
+
         public IndexModel(LexiBalance.Models.LexiBalanceContext context)
         {
             _context = context;
         }
 
-        public IList<Producto> Productos { get; set; }
-
         public async Task OnGetAsync()
         {
-            Productos = await _context.Productos.ToListAsync();
+            var prod = from Producto m in _context.Productos select m;
+            if (!string.IsNullOrEmpty(Buscar))
+            {
+                prod = prod.Where(s => s.Nombre.Contains(Buscar));
+            }
+
+            Productos = await prod.ToListAsync();
         }
     }
 }
