10efc5a4c813b46427ead369577083b16d7653a2
Ana Barja <anabarjafer@gmail.com>
2019-05-07 06:53:06 +0200

    Barra de búsqueda en cliente por teléfono y el personal por DNI.

--- LexiBalance/Pages/Clientes/Index.cshtml.cs
+++ LexiBalance/Pages/Clientes/Index.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿using LexiBalance.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+
 
 namespace LexiBalance.Pages.Clientes
 {
@@ -10,16 +13,24 @@ namespace LexiBalance.Pages.Clientes
     {
         private readonly LexiBalance.Models.LexiBalanceContext _context;
 
+        public IList<Cliente> Cliente { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Buscar { get; set; }
+
         public IndexModel(LexiBalance.Models.LexiBalanceContext context)
         {
             _context = context;
         }
 
-        public IList<Cliente> Cliente { get; set; }
-
         public async Task OnGetAsync()
         {
-            Cliente = await _context.Cliente.ToListAsync();
+            var client = from Cliente m in _context.Cliente select m;
+            if (!string.IsNullOrEmpty(Buscar))
+            {
+                client = client.Where(s => s.Telefono.ToString().Contains(Buscar));
+            }
+
+            Cliente = await client.ToListAsync();
         }
     }
 }
--- LexiBalance/Pages/Personal/Index.cshtml.cs
+++ LexiBalance/Pages/Personal/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using LexiBalance.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace LexiBalance.Pages.Personal
 {
@@ -10,16 +12,24 @@ namespace LexiBalance.Pages.Personal
     {
         private readonly LexiBalance.Models.LexiBalanceContext _context;
 
+        public IList<Trabajador> Trabajador { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Buscar { get; set; }
+
         public IndexModel(LexiBalance.Models.LexiBalanceContext context)
         {
             _context = context;
         }
 
-        public IList<Trabajador> Trabajador { get; set; }
-
         public async Task OnGetAsync()
         {
-            Trabajador = await _context.Trabajador.ToListAsync();
+            var trab = from Trabajador m in _context.Trabajador select m;
+            if (!string.IsNullOrEmpty(Buscar))
+            {
+                trab = trab.Where(s => s.DNI.Contains(Buscar));
+            }
+
+            Trabajador = await trab.ToListAsync();
         }
     }
 }
