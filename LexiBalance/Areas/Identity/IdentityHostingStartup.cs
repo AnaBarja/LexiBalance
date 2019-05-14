5e46b2c784b1ad2bddad85ecee0a783c99f23a1f
Ana Barja <anabarjafer@gmail.com>
2019-05-14 06:53:17 +0200

    Limpieza de código en Usuarios. Como es un página web para gestionar una base de datos no se necesita personalización del usuario. El usuario sólo se registra, entra o sale.

--- LexiBalance/Areas/Identity/IdentityHostingStartup.cs
+++ LexiBalance/Areas/Identity/IdentityHostingStartup.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using LexiBalance.Areas.Identity.Data;
+﻿using LexiBalance.Areas.Identity.Data;
 using LexiBalance.Models;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,7 +12,8 @@ namespace LexiBalance.Areas.Identity
     {
         public void Configure(IWebHostBuilder builder)
         {
-            builder.ConfigureServices((context, services) => {
+            builder.ConfigureServices((context, services) =>
+            {
                 services.AddDbContext<UsuariosContext>(options =>
                     options.UseSqlite(
                         context.Configuration.GetConnectionString("UsuariosContextConnection")));
